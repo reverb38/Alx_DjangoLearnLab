fb94d4d19955667f0850daf576d6b4e96f966dac
reverb38 <zayanalarrey555@gmail.com>
2025-11-19 13:28:30 -1200

    Add README.md and project files

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#hm_(k&764g8wke%n8+fs!e=$*c-)kov!3@2d(2@oagun6*4l8'
+SECRET_KEY = 'django-insecure--^3j&wlnyr9tb$%cs1_9ez7#98+ddr68nrws^qpcslx=9jk63r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ Introduction_to_Django/LibraryProject/README.md
@@ -0,0 +1,8 @@
+# \# LibraryProject
+
+# 
+
+# This is a Django project created for the ALX Introduction to Django module.
+
+# It includes a project named LibraryProject and an application named libraryapp.
+
