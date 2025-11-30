0f47011d576b414861e27011ce06f182660e4937
reverb38 <zayanalarrey555@gmail.com>
2025-11-29 14:16:58 -1200

    Fix: add api and rest_framework to INSTALLED_APPS, add Book model

--- api/models.py
+++ api/models.py
@@ -1,13 +1,8 @@
-# api/models.py
 from django.db import models
 
 class Book(models.Model):
-    title = models.CharField(max_length=200, blank=True, null=True)
-    author = models.CharField(max_length=100, blank=True, null=True)
-    published_date = models.DateField(null=True, blank=True)
-    isbn = models.CharField(max_length=13, unique=True, blank=True, null=True)
-    pages = models.IntegerField(null=True, blank=True)
-    cover = models.URLField(blank=True, null=True)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.title or "Untitled Book"
+        return self.title
