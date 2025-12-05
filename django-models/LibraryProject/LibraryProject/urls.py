e3c93d9fa7bca24a3ed64654e24d7f69bfd9d632
reverb38 <zayanalarrey555@gmail.com>
2025-12-05 02:25:02 -1200

    changes Made

--- django-models/LibraryProject/LibraryProject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # include lets us add app URLs
-
-urlpatterns = [
-    path('admin/', admin.site.urls),                # default admin
-    path('', include('relationship_app.urls')),    # include app URLs
-]
--- django-models/LibraryProject/README.md
+++
@@ -1,8 +0,0 @@
-# \# LibraryProject
-
-# 
-
-# This is a Django project created for the ALX Introduction to Django module.
-
-# It includes a project named LibraryProject and an application named libraryapp.
-
