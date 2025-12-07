650b42838eb1e0988b89d7db0e5da8c1c1673b88
reverb38 <zayanalarrey555@gmail.com>
2025-12-06 20:31:04 -1200

    Register Book model and customize Django admin

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # other apps...
     'bookshelf',
     'relationship_app',
 
